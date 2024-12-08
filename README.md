00a5228fdf8afdbbb54fd65ecd5c38558d98c6d6
Seth R <sar1074@unh.edu>
2024-12-08 14:53:58 -0500

    adding file/dir omission and polishing everything up

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # project-breakdown
 simple utility that takes a large project folder and finds connections between files
+
+**why?**
+I was working on a large pre-existing robotics project utilizing ros2 and needed an easy way to see what files depended on what.
--- project_breakdown.py
+++ project_breakdown.py
@@ -25,14 +25,34 @@ colors = {
 	'folder': '\033[0;35m'
 }
 
-dir_to_search = "/Users/sethrossman/Desktop/UNH Y4/ASV/mantaray-ground-control/"
+dir_to_search = "/Users/sethrossman/Desktop/UNH Y4/ASV"
+
+files_to_omit = [".DS_Store", ".gitignore"]
+dir_to_omit = [".git", "__pycache__", ".vscode"]
+
+def omitted(path, to_omit):
+	for omit in to_omit:
+		if path.find(omit) != -1: # directory not in omit list
+			return True
+	return False
 
 def search_code():
 	all_files = {}
 	for dirpath, dirnames, filenames in os.walk(dir_to_search, topdown=True):
-		for f in filenames:
-			# list containing all connected nodes
-			all_files[f"{dirpath}/{f}"] = 0
+		# for f in filenames:
+		# 	# list containing all connected nodes
+		# 	all_files[f"{dirpath}/{f}"] = {
+		# 		'num_of_instances': 0,
+		# 		'connections': []
+		# 	}
+		if not omitted(dirpath, dir_to_omit):
+			for f in filenames:
+				# list containing all connected nodes (minus files_to_omit)
+				if not omitted(f, files_to_omit):
+					all_files[f"{dirpath}/{f}"] = {
+						'num_of_instances': 0,
+						'connections': []
+					}
 			# print(f"{dirpath}/{f}")
 
 	for key in all_files.keys():
@@ -40,18 +60,19 @@ def search_code():
 		with open(key, 'r') as ff:
 			try:
 				file_read = ff.read()
-				# print(file_read)
+				# if a file 'k' is referenced in another file, add to that files connections
 				for k in all_files.keys():
-					# print(k)
 					search_k = k.split("/")[-1]
-					# print(search_k)
 					search_k_search = search_k.split(".")
+					# print(f"search_k: {search_k}, search_k_search: {search_k_search}")
 					if len(search_k_search) == 1:
 						if file_read.find(search_k_search) != -1:
-							all_files[key] += 1
+							all_files[key]['num_of_instances'] += 1
+							all_files[key]['connections'].append(k)
 					else:
 						if file_read.find(search_k_search[-2]) != -1:
-							all_files[key] += 1
+							all_files[key]['num_of_instances'] += 1
+							all_files[key]['connections'].append(k)
 			except:
 				# trying to read an unreadable file
 				# print("---")
@@ -65,28 +86,36 @@ def print_res(connections_dict):
 		indentation = "".join([' ' for i in range(level)])
 
 		aa = dirpath.split("/")[-1]
-		print(f"{indentation}{colors['root_f']}/{aa}{colors['white']}")
-		for folder in dirnames:
-			print(f"{indentation}{colors['folder']}  {folder}{colors['white']}")
-		# print(filenames)
-		for f in filenames:
-			f_split = f.split(".")
-			if (len(f_split) == 2):
-				if f_split[1] == "py":
-					print(f"{indentation}  {colors['python']}{f}{colors['white']}", end='')
-				elif f_split[1] == "ino":
-					print(f"{indentation}  {colors['arduino']}{f}{colors['white']}", end='')
-				elif f_split[1] == "png":
-					print(f"{indentation}  {colors['png']}{f}{colors['white']}", end='')
-				else:
-					print(f"{indentation}  {f}", end='')
-			else:
-				print(f"{indentation}  {f}", end='')
-			connected_instances = connections_dict[f"{dirpath}/{f}"]
-			if (connected_instances != 0):
-				print(f"{colors['connection']} ({connected_instances}){colors['white']}")
-			else:
-				print()
+
+		# NOTE: possibly change so ONLY the root omitted folder is displayed
+		if not omitted(dirpath, dir_to_omit):
+			print(f"{indentation}{colors['root_f']}/{aa}{colors['white']}")
+
+			for folder in dirnames:
+				print(f"{indentation}{colors['folder']}  {folder}{colors['white']}")
+
+			for f in filenames:
+				if not omitted(f, files_to_omit):
+					f_split = f.split(".")
+					if (len(f_split) == 2):
+						if f_split[1] == "py":
+							print(f"{indentation}  {colors['python']}{f}{colors['white']}", end='')
+						elif f_split[1] == "ino":
+							print(f"{indentation}  {colors['arduino']}{f}{colors['white']}", end='')
+						elif f_split[1] == "png":
+							print(f"{indentation}  {colors['png']}{f}{colors['white']}", end='')
+						else:
+							print(f"{indentation}  {f}", end='')
+					else:
+						print(f"{indentation}  {f}", end='')
+
+					connected_instances = connections_dict[f"{dirpath}/{f}"]
+					if (connected_instances['num_of_instances'] != 0):
+						print(f"{colors['connection']} ({connected_instances['num_of_instances']}){colors['white']}")
+						for connect in connected_instances['connections']:
+							print(f"{indentation}  | {colors['connection']}{connect}{colors['white']}")
+					else:
+						print()
 
 def main():
 	connections_dict = search_code()
